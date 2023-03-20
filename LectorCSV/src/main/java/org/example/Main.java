ecb1c5a3f3f9dfe0c796f84aad5d80d6ac0a38ae
Brunoat81 <brunoat@hotmail.co.uk>
2023-03-20 00:33:34 -0300

    Update Main.java

--- LectorCSV/src/main/java/org/example/Main.java
+++ LectorCSV/src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 import java.util.List;
 
 import com.opencsv.bean.CsvToBeanBuilder;
-import modelo.ServicioContratado;
+
+import modelo.Partidos;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,23 +15,39 @@ public class Main {
             System.exit(88);
         }
 
-        List <ServicioContratado> listaDeSuscripciones;
+        List <Partidos> listaDePartidos;
         try {
             // En esta primera línea definimos el archivos que va a ingresar
-           listaDeSuscripciones = new CsvToBeanBuilder(new FileReader(args[0]))
+           listaDePartidos = new CsvToBeanBuilder(new FileReader(args[0]))
                       // Es necesario definir el tipo de dato que va a generar el objeto que estamos queriendo parsear a partir del CSV
-                    .withType(ServicioContratado.class)
+                    .withType(Partidos.class)
                     .build()
                     .parse();
 
               //El resultado de este método nos da una lita del objetos
 
-            for (ServicioContratado suscripcion : listaDeSuscripciones) {
-                System.out.println(suscripcion.getSitio() + ";" + suscripcion.getServicioDeContenido() + ";" +
-                        suscripcion.getIdServicioDeContenido() + ";" + suscripcion.getFechaDeAlta() + ";" + suscripcion.getEstado());
+            for (Partidos partidos : listaDePartidos) {
+                String ganador = null;
+                String ganador1 = null;
+                
+                if (partidos.getGolesEquipoA() > partidos.getGolesEquipoB()) {
+                	ganador = partidos.getEquipoA();
+                    System.out.println ("El ganador es: " + ganador);
+                } else if (partidos.getGolesEquipoA() < partidos.getGolesEquipoB()){
+                	ganador = partidos.getEquipoB ();
+                    System.out.println ("El ganador es: " + ganador);
+                } else {
+                	ganador = partidos.getEquipoA();
+                	ganador1 = partidos.getEquipoB();
+                    System.out.println ("Empate entre: " + ganador + " y " + ganador1);
+                }
+
+
             }
         } catch (IOException e) {
             e.printStackTrace();
+	}
     }
+
 }
-}
+    
