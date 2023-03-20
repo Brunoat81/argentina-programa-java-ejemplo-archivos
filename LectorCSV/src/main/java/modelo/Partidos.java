99286c3da98b6a8861bd08140b90a8c1ecefb351
Brunoat81 <brunoat@hotmail.co.uk>
2023-03-20 00:33:16 -0300

    Create Partidos.java

---
+++ LectorCSV/src/main/java/modelo/Partidos.java
@@ -0,0 +1,51 @@
+package modelo;
+
+import com.opencsv.bean.CsvBindByPosition;
+
+public class Partidos {
+	
+    @CsvBindByPosition(position = 0)
+    private String EquipoA;
+
+	@CsvBindByPosition(position = 1)
+    private Integer golesEquipoA;
+    
+    @CsvBindByPosition(position = 2)
+    private String EquipoB;
+    
+    @CsvBindByPosition(position = 3)
+    private Integer golesEquipoB;
+    
+    public String getEquipoA() {
+		return EquipoA;
+	}
+
+	public void setEquipoA(String equipoA) {
+		EquipoA = equipoA;
+	}
+
+	public Integer getGolesEquipoA() {
+		return golesEquipoA;
+	}
+
+	public void setGolesEquipoA(Integer golesEquipoA) {
+		this.golesEquipoA = golesEquipoA;
+	}
+
+	public String getEquipoB() {
+		return EquipoB;
+	}
+
+	public void setEquipoB(String equipoB) {
+		EquipoB = equipoB;
+	}
+
+	public Integer getGolesEquipoB() {
+		return golesEquipoB;
+	}
+
+	public void setGolesEquipoB(Integer golesEquipoB) {
+		this.golesEquipoB = golesEquipoB;
+	}
+    
+}
