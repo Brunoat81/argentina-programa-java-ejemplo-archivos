6966939186a9e2c7c482a3ee5bf4b736263bc853
Brunoat81 <brunoat@hotmail.co.uk>
2023-03-21 13:44:28 -0300

    Update Main.java

--- LectorCSV/src/main/java/org/example/Main.java
+++ LectorCSV/src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import modelo.Partidos;
+import modelo.Pronosticos;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,6 +17,8 @@ public class Main {
         }
 
         List <Partidos> listaDePartidos;
+        List <Pronosticos> listaDePronosticos;
+        
         try {
             // En esta primera línea definimos el archivos que va a ingresar
            listaDePartidos = new CsvToBeanBuilder(new FileReader(args[0]))
@@ -24,30 +27,57 @@ public class Main {
                     .build()
                     .parse();
 
+           listaDePronosticos = new CsvToBeanBuilder(new FileReader(args[0]))
+        		   
+        		   .withType(Pronosticos.class)
+        		   .build()
+        		   .parse();
+           
               //El resultado de este método nos da una lita del objetos
-
+           
             for (Partidos partidos : listaDePartidos) {
                 String ganador = null;
                 String ganador1 = null;
+                int cont = 0;
+                String gana = "gana";
+                String empata = "empata";
+                
                 
                 if (partidos.getGolesEquipoA() > partidos.getGolesEquipoB()) {
                 	ganador = partidos.getEquipoA();
                     System.out.println ("El ganador es: " + ganador);
+                    
+                    for (Pronosticos pronosticos : listaDePronosticos) {                    
+                        if (pronosticos.getPronosticoEquipoA().equals(gana)) {
+                           	cont = cont +1;
+                        }
+                    }
                 } else if (partidos.getGolesEquipoA() < partidos.getGolesEquipoB()){
                 	ganador = partidos.getEquipoB ();
                     System.out.println ("El ganador es: " + ganador);
+                    
+                    for (Pronosticos pronosticos : listaDePronosticos) {                    
+                        if (pronosticos.getPronosticoEquipoB().equals(gana)) {
+                           	cont = cont + 1;
+                        }
+                    }
                 } else {
                 	ganador = partidos.getEquipoA();
                 	ganador1 = partidos.getEquipoB();
                     System.out.println ("Empate entre: " + ganador + " y " + ganador1);
-                }
-
-
+                    
+                    for (Pronosticos pronosticos : listaDePronosticos) {                    
+                        if (pronosticos.getPronosticoEquipoA().equals(empata)) {
+                           	cont = cont + 1;
+                        }
+                    }
             }
+                System.out.println("El puntaje obtenido por pronósticos correctos es: " + cont);
+            }
+                   
         } catch (IOException e) {
             e.printStackTrace();
 	}
     }
-
 }
     
